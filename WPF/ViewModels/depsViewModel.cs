ce1119273efcab8a34b914f0fa803735b4200e0c
Konstantin Antsiferov <kanciferov@mail.ru>
2016-11-22 17:32:02 +0300

    222

---
+++ WPF/ViewModels/depsViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF.ViewModels
+{
+    class depsViewModel : ViewModelBase<BL.Dep>
+    {
+        public depsViewModel()
+        {
+            SelectionMode = true;
+        }
+        protected override bool Filter(object item)
+        {
+            BL.Dep dep = item as BL.Dep;
+            bool result = true;
+            if (!string.IsNullOrWhiteSpace(FindText) && 
+                !(dep.Name ?? string.Empty).ToLower().Contains(FindText.ToLower()) && 
+                !(dep.Id.ToString() ?? string.Empty).ToLower().Contains(FindText.ToLower())
+                )
+            {
+                result = false;
+            }
+            return result;
+        }
+    }
+}
---
+++ WPF/ViewModels/possViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF.ViewModels
+{
+    class possViewModel : ViewModelBase<BL.Pos>
+    {
+        protected override bool Filter(object item)
+        {
+            BL.Pos pos = item as BL.Pos;
+            bool result = true;
+            if (!string.IsNullOrWhiteSpace(FindText) &&
+                !(pos.Name ?? string.Empty).ToLower().Contains(FindText.ToLower()) &&
+                !(pos.Id.ToString() ?? string.Empty).ToLower().Contains(FindText.ToLower())
+                )
+            {
+                result = false;
+            }
+            return result;
+        }
+    }
+}
