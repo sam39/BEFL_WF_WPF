0d43f5887d0edad44a7bc5cef39e6988a73306ef
Konstantin Antsiferov <kanciferov@mail.ru>
2016-12-28 11:24:44 +0300

    AspNotWork

--- WebSpr/Global.asax.cs
+++ WebSpr/Global.asax.cs
@@ -17,7 +17,6 @@ namespace BEFLSPR
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
